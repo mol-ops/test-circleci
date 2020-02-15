117cb5d0c28f0336995e1b6c2215efeadcddc79c
Hengfeng Li <hengf.li@gmail.com>
2020-02-16 00:27:29 +1100

    refine a bit.

--- foo_test.go
+++ foo_test.go
@@ -9,5 +9,8 @@ import (
 func TestFoo(t *testing.T) {
 	fmt.Println("Test foo is called.")
 	env := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+	if env == "" {
+		env = "default_value"
+	}
 	fmt.Println("FOO:", env[:5])
 }
